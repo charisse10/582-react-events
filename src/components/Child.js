9d67cc4e25586d977e4695e7fecce55c5a623480
VANIERCOLLEGE\1995510 <1995510@edu.vaniercollege.qc.ca>
2023-09-05 15:05:40 -0400

    On click handler

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,11 +1,16 @@
 function Child(props) {
+
+    function onClickHandler(e) {
+        console.log(`clicked ${e.target}`)
+    }
+
     return (
         <div className = {props.className ? props.className : "default-class"}>
             <h3>I am a child component</h3>
-             {props.name ? <h3>NAME: {props.name}</h3> : <h3>No name passed</h3>}
+             {props.name ? (<h3 onClick={onClickHandler}>NAME: {props.name}</h3>) : (<h3>No name passed</h3>)}
              {props.obj ? (
                 <>
-             <h3>
+             <h3 onClick={onClickHandler}>
                OBJECT: {props.obj.id} - {props.obj.name}
              </h3>
             <p>{props.obj.items}</p>
